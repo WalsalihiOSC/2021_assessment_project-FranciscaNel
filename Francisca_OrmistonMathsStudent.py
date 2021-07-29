5f4dea06d7834315262a132425171a6853b2fa80
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-07-29 12:12:34 +1200

    Ver. 2

--- Francisca_OrmistonMathsStudent.py
+++ Francisca_OrmistonMathsStudent.py
@@ -4,13 +4,17 @@
 # Ver 1
 
 class Student:
-    def __init__(self,sn,ss,sa):
+    def __init__(self,sn):
 
     # defining (possible) variables for the student instance
         self.student_name = sn
-        self.student_score = ss
-        self.student_attempts = sa
+        #self.student_score = ss
+        #self.student_attempts = sa
+        self.questiontypes = [] 
+        #self.questiontypes = qt
+
+        self.qtypes = ['Addition +','Subtraction -','Times x','Division ÷']
 
     # These variables would be for recording the personal leaderboard
-        self.student_attempts = len(self.scores_list)
-        self.scores_list = []
+        #self.student_attempts = len(self.scores_list)
+        #self.scores_list = []
