0dc83a62e32cb5bdb99c0f4996e87f584ac8a411
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-07-29 12:13:31 +1200

    Ver.6

--- Francisca_OrmistonMathsInterface.py
+++ Francisca_OrmistonMathsInterface.py
@@ -14,6 +14,7 @@ root.geometry("880x495") # small 16:9 ratio window (55x)
 class Interface:
     def __init__(self):
 
+
 ####### PAGE 1 #######
 
         self.name_page = Frame(root, bg='#EEEEEE')
@@ -31,11 +32,20 @@ class Interface:
     # Text boxes
         self.student_name = Entry(self.name_page, bg="#434343", fg='#efefef', border=0, highlightbackground = "#434343", highlightthickness=5)
         self.student_name.grid(row=2,padx=(120,0))
+
     # Buttons
-        self.nextbutton = Button(self.name_page,command=lambda:[self.get_name(),self.next_page(self.name_page),self.next_page(self.logotitle),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=3,column=2,pady=(50,0))
+        Button(self.name_page,command=lambda:[self.check_name_input(self.name_page),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=3,column=2,pady=(50,0))
 
 
 ####### FUNCTIONS TO BE USED #######
+
+# Checks if the name has input. (there is no checking of integer input in the name)
+    def check_name_input(self,cpage):
+        if not self.student_name.get():
+            print('enter a name please')
+        else:
+            self.get_name()
+            self.next_page(cpage)
 
 # Formats each page with the same font and packs it
     def formatting(self,page):
@@ -51,46 +61,81 @@ class Interface:
         sn = self.student_name.get()
         self.student = Student(sn)
 
+
 ####### PAGE 2 #######
 
     def selection(self):
         self.selection_page = Frame(root, bg='#EEEEEE')
         self.formatting(self.selection_page)
-        self.selection_page.grid_columnconfigure((0, 1, 2), weight=1)
         self.selection_page.grid_configure(padx=(0,0),pady=(20,0))
         print('name is:',self.student.student_name) # checking if name is saved to student class
 
-    # button iteration
+        self.qtypesbuttons = [] # qtypes = question types, i.e: addition, subtraction...
+        self.diffbuttons = [] # diff = difficulty, i.e: easy, hard...
 
+    # Button selection for question types
+        self.selectionfunc(self.student.qtypes,self.qtypesbuttons,self.student.selected_questiontype,1)
+    # Button selection for difficulties
+        self.selectionfunc(self.student.difficulties,self.diffbuttons,self.student.selected_difficulty,2)
+    # next button
+        Button(self.selection_page,command=lambda:[self.check_qbutton_selected(self.selection_page),self.question1()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=3,column=4,pady=(50,0))
+
+# Function for selection of buttons to be used with question types and difficulties
+    def selectionfunc(self,to_select,buttonlist,selection,row):
+    # labels 
         i = 0 # index
         c = 0 # column
-        r = 0 # row
+        r = row # row
 
-        self.qtypesbuttons = [] # list for buttons
-
-        for i in range(len(self.student.qtypes)): # laying out the buttons using iteration
-            t = self.student.qtypes[i]
-            self.qtypesbuttons.append(Button(self.selection_page, text=t, command = lambda p=i: self.button_pressed(p)))
+        for i in range(len(to_select)):
+            t = to_select[i]
+            buttonlist.append(Button(self.selection_page, text=t, command = lambda p=i: self.button_pressed(p,to_select,buttonlist,selection)))
             if c < 4:
-                c = c + 1 # for each button, the column is increased by 1
+                c = c + 1
 
-            self.qtypesbuttons[i].grid(column=c, row=r, padx=(8),pady=8)
-            self.qtypesbuttons[i].config(width=10,bg="#ff9900", fg='#434343', border=0, highlightthickness=4)
+            buttonlist[i].grid(column=c, row=r, padx=(8),pady=8)
+            buttonlist[i].config(width=10,bg="#ff9900", fg='#434343', border=0, highlightthickness=4)
 
 # checks if button is pressed by detecting button colour as green or orange
-    def button_pressed(self,sel):
+    def button_pressed(self,sel,to_select,buttonlist,selection):
         print(sel)
-        b = self.qtypesbuttons[sel]
-        selq = self.student.qtypes[sel]
+        b = buttonlist[sel]
+        selq = to_select[sel]
     # If the button is clicked while green, the selection is removed from the list
         if b.cget('bg') == "#64a335" :
             b.config(bg="#ff9900")
-            self.student.questiontypes.remove(selq)   
+            selection.remove(selq)   
     # If the button is clicked while orange, it is turned green and added to the list
-        elif len(self.student.questiontypes) < 1:
+        elif len(selection) < 1:
             b.config(bg="#64a335")
-            self.student.questiontypes.append(selq)
-        print(self.student.questiontypes)
+            selection.append(selq)
+        print(selection)
+
+# checks whether there was a selection for the question type and the difficulty lists
+    def check_qbutton_selected(self,cpage): # cpage = current page
+        if len(self.student.selected_questiontype) == 0:
+            print('please select a question type')
+        else:
+            self.check_dbutton_selected(cpage)
+    def check_dbutton_selected(self,cpage):
+        if len(self.student.selected_difficulty) == 0:
+            print('please select a difficulty')
+        else:
+            self.next_page(cpage)
+
+####### PAGE 3 #######
+    def question1(self):
+        self.question1_page = Frame(root, bg='#EEEEEE')
+        self.formatting(self.question1_page)
+
+        #self.qtypes = ['Addition +','Subtraction -','Times x','Division ÷']
+        #self.difficulties = ['Easy','Intermediate','Hard']
+
+        self.questiontype = str(self.student.selected_questiontype[0])
+        self.difficulty = str(self.student.selected_difficulty[0])
+
+        print(self.questiontype,'\n',self.difficulty)
+
 
 Interface()
 root.mainloop() 
