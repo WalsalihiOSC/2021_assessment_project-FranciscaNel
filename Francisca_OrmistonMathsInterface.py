4b95f0f93b531bfb40ab9e87bddecb348830dd92
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-07-29 12:11:09 +1200

    Ver.5

--- Francisca_OrmistonMathsInterface.py
+++ Francisca_OrmistonMathsInterface.py
@@ -1,7 +1,7 @@
 # CSC3 2021
 # Ormiston Computing Interface Class
 # Francisca Nel
-# Ver 3
+# Ver 5
 
 from Francisca_OrmistonMathsStudent import Student
 from tkinter import *
@@ -32,7 +32,7 @@ class Interface:
         self.student_name = Entry(self.name_page, bg="#434343", fg='#efefef', border=0, highlightbackground = "#434343", highlightthickness=5)
         self.student_name.grid(row=2,padx=(120,0))
     # Buttons
-        self.nextbutton = Button(self.name_page,command=lambda:[self.get_name,self.next_page(self.name_page),self.next_page(self.logotitle),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=3,column=2,pady=(50,0))
+        self.nextbutton = Button(self.name_page,command=lambda:[self.get_name(),self.next_page(self.name_page),self.next_page(self.logotitle),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=3,column=2,pady=(50,0))
 
 
 ####### FUNCTIONS TO BE USED #######
@@ -57,24 +57,40 @@ class Interface:
         self.selection_page = Frame(root, bg='#EEEEEE')
         self.formatting(self.selection_page)
         self.selection_page.grid_columnconfigure((0, 1, 2), weight=1)
-        self.selection_page.grid_configure(padx=(50,0),pady=(20,0))
+        self.selection_page.grid_configure(padx=(0,0),pady=(20,0))
+        print('name is:',self.student.student_name) # checking if name is saved to student class
 
-    # labels - There is a more efficient way of doing this, but the buttons will be line-by-line for now.
+    # button iteration
 
-        Label(self.selection_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').grid() # Title
-    # Question Type Selection.pack
-        Label(self.selection_page,text="Question Type",bg='#efefef',fg='#434343').grid(row=1)
-        Button(self.selection_page, text="Addition +",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=2,column=0,sticky=W)
-        Button(self.selection_page, text="Subtraction −",fg='#434343',bg="#ff9900", border=0, height=2, width=11).grid(row=2,column=0,padx=(188,0),sticky=W)
-        Button(self.selection_page, text="Times X",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=2,column=0,padx=(414,0),sticky=W)
-        Button(self.selection_page, text="Division ÷",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=2,column=0,padx=(602,0),sticky=W)
-    # Difficulty Level Selection
-        Label(self.selection_page,text="Difficulty Level",bg='#efefef',fg='#434343').grid(row=6)
-        Button(self.selection_page, text="Easy",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=7,column=0,padx=(90,0),sticky=W)
-        Button(self.selection_page, text="Intermediate",fg='#434343',bg="#ff9900", border=0, height=2, width=11).grid(row=7,column=0,padx=(280,0),sticky=W)
-        Button(self.selection_page, text="Hard",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=7,column=0,padx=(508,0),sticky=W)
-        
-        self.nextbutton = Button(self.selection_page, text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=10,pady=(20,0),sticky=E)
- 
+        i = 0 # index
+        c = 0 # column
+        r = 0 # row
+
+        self.qtypesbuttons = [] # list for buttons
+
+        for i in range(len(self.student.qtypes)): # laying out the buttons using iteration
+            t = self.student.qtypes[i]
+            self.qtypesbuttons.append(Button(self.selection_page, text=t, command = lambda p=i: self.button_pressed(p)))
+            if c < 4:
+                c = c + 1 # for each button, the column is increased by 1
+
+            self.qtypesbuttons[i].grid(column=c, row=r, padx=(8),pady=8)
+            self.qtypesbuttons[i].config(width=10,bg="#ff9900", fg='#434343', border=0, highlightthickness=4)
+
+# checks if button is pressed by detecting button colour as green or orange
+    def button_pressed(self,sel):
+        print(sel)
+        b = self.qtypesbuttons[sel]
+        selq = self.student.qtypes[sel]
+    # If the button is clicked while green, the selection is removed from the list
+        if b.cget('bg') == "#64a335" :
+            b.config(bg="#ff9900")
+            self.student.questiontypes.remove(selq)   
+    # If the button is clicked while orange, it is turned green and added to the list
+        elif len(self.student.questiontypes) < 1:
+            b.config(bg="#64a335")
+            self.student.questiontypes.append(selq)
+        print(self.student.questiontypes)
+
 Interface()
 root.mainloop() 
