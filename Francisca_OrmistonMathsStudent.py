fa6062b6f227c270f02cb99973964fc67c759621
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-07-29 12:17:38 +1200

    Ver.5

--- Francisca_OrmistonMathsStudent.py
+++ Francisca_OrmistonMathsStudent.py
@@ -7,13 +7,16 @@ class Student:
     def __init__(self,sn):
     # defining (possible) variables for the student instance
         self.student_name = sn
-    # for storing questiontype/difficulty temporarily to check if selection < 1
+    # Lists for storing questiontype/difficulty temporarily to check if selection < 1
         self.selected_questiontype = [] 
         self.selected_difficulty = []
-    # List of all attempted question types and difficulties from a user.
+    # Lists to store all attempted question types and difficulties from a user.
         self.selected_questiontypes = [] 
         self.selected_difficulties = []
-    # List of all incorrect answers and correct answers for the scores
+    # Lists to store incorrect answers and correct answers for the scores
         self.incorrect_answers = []
         self.correct_answers = []
         self.scores = []
+    # Lists for question types and difficulties
+        self.qtypes = ['Addition +','Times x','Division ÷','Subtraction -']
+        self.difficulties = ['Easy','Intermediate','Hard']
