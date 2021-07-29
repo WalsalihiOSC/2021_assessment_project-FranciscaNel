fff58ee692a938d611c40892bd9306d3cb717aa9
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-07-29 12:09:04 +1200

    Ver.4

--- Francisca_OrmistonMathsInterface.py
+++ Francisca_OrmistonMathsInterface.py
@@ -11,27 +11,28 @@ root.configure(bg='#EEEEEE')
 root.title("Ormiston Maths")
 root.geometry("880x495") # small 16:9 ratio window (55x)
 
-####### PAGE 1 #######
-
 class Interface:
     def __init__(self):
+
+####### PAGE 1 #######
+
         self.name_page = Frame(root, bg='#EEEEEE')
         self.formatting(self.name_page)
        
-    # OSC logo displayed using PhotoImage. 
+    # OSC logo displayed using PhotoImage.
         self.img = PhotoImage(file="OJC_logo3.gif")
         self.logotitle = Label(self.name_page, image=self.img, bg='#EEEEEE')
         self.logotitle.photo = self.img
-        self.logotitle.pack(side=TOP)
-
+        self.logotitle.grid()
+    # Title
+        Label(self.name_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').grid(row=1)
     # Labels
-        Label(self.name_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').pack()
-        Label(self.name_page,text="Name",bg='#efefef',fg='#434343').pack(side=LEFT,pady=(0,60),padx=(20,20)) 
+        Label(self.name_page,text="Name",bg='#efefef',fg='#434343').grid(row=2,pady=(20),padx=(0,400))
     # Text boxes
         self.student_name = Entry(self.name_page, bg="#434343", fg='#efefef', border=0, highlightbackground = "#434343", highlightthickness=5)
-        self.student_name.pack(pady=(20,0))
+        self.student_name.grid(row=2,padx=(120,0))
     # Buttons
-        self.nextbutton = Button(self.name_page,command=lambda:[self.get_name,self.next_page(self.name_page),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).pack(side=RIGHT,pady=(40,0))
+        self.nextbutton = Button(self.name_page,command=lambda:[self.get_name,self.next_page(self.name_page),self.next_page(self.logotitle),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=3,column=2,pady=(50,0))
 
 
 ####### FUNCTIONS TO BE USED #######
@@ -39,11 +40,11 @@ class Interface:
 # Formats each page with the same font and packs it
     def formatting(self,page):
         page.option_add('*Font', 'CenturyGothic 24 bold')
-        page.pack()
+        page.grid(row=0,column=0,padx=(170,0))
 
 # Next page function- forgets the current page
     def next_page(self,cpage):
-        cpage.pack_forget()
+        cpage.grid_forget()
 
 # Function that collects the student's name
     def get_name(self): 
@@ -55,26 +56,25 @@ class Interface:
     def selection(self):
         self.selection_page = Frame(root, bg='#EEEEEE')
         self.formatting(self.selection_page)
+        self.selection_page.grid_columnconfigure((0, 1, 2), weight=1)
+        self.selection_page.grid_configure(padx=(50,0),pady=(20,0))
 
-    # labels - There is a much more efficient way of doing this, but the buttons will be line-by-line for now.
-    # Because I am using pack, I have to make a seperate frame for a different row...
+    # labels - There is a more efficient way of doing this, but the buttons will be line-by-line for now.
 
-        Label(self.selection_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').pack(pady=10) # Title
-    # Question Type Selection
-        Label(self.selection_page,text="Question Type",bg='#efefef',fg='#434343').pack()
-        Button(self.selection_page, text="Addition +",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
-        Button(self.selection_page, text="Subtraction −",fg='#434343',bg="#ff9900", border=0, height=2, width=11).pack(side=LEFT)
-        Button(self.selection_page, text="Times X",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
-        Button(self.selection_page, text="Division ÷",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT)
+        Label(self.selection_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').grid() # Title
+    # Question Type Selection.pack
+        Label(self.selection_page,text="Question Type",bg='#efefef',fg='#434343').grid(row=1)
+        Button(self.selection_page, text="Addition +",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=2,column=0,sticky=W)
+        Button(self.selection_page, text="Subtraction −",fg='#434343',bg="#ff9900", border=0, height=2, width=11).grid(row=2,column=0,padx=(188,0),sticky=W)
+        Button(self.selection_page, text="Times X",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=2,column=0,padx=(414,0),sticky=W)
+        Button(self.selection_page, text="Division ÷",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=2,column=0,padx=(602,0),sticky=W)
     # Difficulty Level Selection
-        self.buttons2 = Frame(root,bg='#EEEEEE')
-        self.formatting(self.buttons2)
-        Label(self.buttons2,text="Difficulty Level",bg='#efefef',fg='#434343').pack(side=TOP,pady=(15,0))
-        Button(self.buttons2, text="Easy",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
-        Button(self.buttons2, text="Intermediate",fg='#434343',bg="#ff9900", border=0, height=2, width=11).pack(side=LEFT)
-        Button(self.buttons2, text="Hard",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
+        Label(self.selection_page,text="Difficulty Level",bg='#efefef',fg='#434343').grid(row=6)
+        Button(self.selection_page, text="Easy",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=7,column=0,padx=(90,0),sticky=W)
+        Button(self.selection_page, text="Intermediate",fg='#434343',bg="#ff9900", border=0, height=2, width=11).grid(row=7,column=0,padx=(280,0),sticky=W)
+        Button(self.selection_page, text="Hard",fg='#434343',bg="#ff9900", border=0, height=2, width=9).grid(row=7,column=0,padx=(508,0),sticky=W)
         
-        self.nextbutton = Button(self.buttons2, text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).pack(side=BOTTOM,pady=(40,0))
+        self.nextbutton = Button(self.selection_page, text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).grid(row=10,pady=(20,0),sticky=E)
  
 Interface()
 root.mainloop() 
