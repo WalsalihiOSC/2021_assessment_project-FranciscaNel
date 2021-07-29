a79c3b0391bad5005188585ec5e296ee28d8abb7
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-07-29 12:07:55 +1200

    Ver.3

--- Francisca_OrmistonMathsInterface.py
+++ Francisca_OrmistonMathsInterface.py
@@ -1,7 +1,7 @@
 # CSC3 2021
 # Ormiston Computing Interface Class
 # Francisca Nel
-# Ver 2
+# Ver 3
 
 from Francisca_OrmistonMathsStudent import Student
 from tkinter import *
@@ -28,7 +28,8 @@ class Interface:
         Label(self.name_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').pack()
         Label(self.name_page,text="Name",bg='#efefef',fg='#434343').pack(side=LEFT,pady=(0,60),padx=(20,20)) 
     # Text boxes
-        self.student_name=Entry(self.name_page, bg="#434343", fg='#efefef', border=0, highlightbackground = "#434343", highlightthickness=5).pack(pady=(20,0))
+        self.student_name = Entry(self.name_page, bg="#434343", fg='#efefef', border=0, highlightbackground = "#434343", highlightthickness=5)
+        self.student_name.pack(pady=(20,0))
     # Buttons
         self.nextbutton = Button(self.name_page,command=lambda:[self.get_name,self.next_page(self.name_page),self.selection()], text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).pack(side=RIGHT,pady=(40,0))
 
@@ -55,8 +56,25 @@ class Interface:
         self.selection_page = Frame(root, bg='#EEEEEE')
         self.formatting(self.selection_page)
 
-        Label(self.name_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').pack()
-        
+    # labels - There is a much more efficient way of doing this, but the buttons will be line-by-line for now.
+    # Because I am using pack, I have to make a seperate frame for a different row...
 
+        Label(self.selection_page,text="Ormiston Maths",bg='#efefef',fg='#ff9900',font='CenturyGothic 48 bold').pack(pady=10) # Title
+    # Question Type Selection
+        Label(self.selection_page,text="Question Type",bg='#efefef',fg='#434343').pack()
+        Button(self.selection_page, text="Addition +",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
+        Button(self.selection_page, text="Subtraction −",fg='#434343',bg="#ff9900", border=0, height=2, width=11).pack(side=LEFT)
+        Button(self.selection_page, text="Times X",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
+        Button(self.selection_page, text="Division ÷",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT)
+    # Difficulty Level Selection
+        self.buttons2 = Frame(root,bg='#EEEEEE')
+        self.formatting(self.buttons2)
+        Label(self.buttons2,text="Difficulty Level",bg='#efefef',fg='#434343').pack(side=TOP,pady=(15,0))
+        Button(self.buttons2, text="Easy",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
+        Button(self.buttons2, text="Intermediate",fg='#434343',bg="#ff9900", border=0, height=2, width=11).pack(side=LEFT)
+        Button(self.buttons2, text="Hard",fg='#434343',bg="#ff9900", border=0, height=2, width=9).pack(side=LEFT,padx=15)
+        
+        self.nextbutton = Button(self.buttons2, text="Next",fg='#434343',bg="#78c043", border=0, height=1, width=8).pack(side=BOTTOM,pady=(40,0))
+ 
 Interface()
-root.mainloop()
+root.mainloop() 
