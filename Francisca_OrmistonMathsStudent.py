df85245660d74ccbb2a272f5394cdded9a265fc9
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-08-20 14:29:36 +1200

    Ver.7

--- Francisca_OrmistonMathsStudent.py
+++ Francisca_OrmistonMathsStudent.py
@@ -1,7 +1,7 @@
 # CSC3 2021
 # Ormiston Computing Student Class
 # Francisca Nel
-# Ver 6
+# Ver 7
 
 class Student:
     def __init__(self,sn):
@@ -10,20 +10,16 @@ class Student:
     # Lists for storing questiontype/difficulty temporarily to check if selection < 1
         self.selected_questiontype = [] 
         self.selected_difficulty = []
-    # Lists to store all attempted question types and difficulties from a user.
-        self.selected_questiontypes = [] 
-        self.selected_difficulties = []
     # Lists to store incorrect answers and correct answers for the scores
         self.correct_answers = []
         self.score = 0
     # Lists for question types and difficulties
-        self.qtypes = ['Addition +','Times x','Division ÷','Subtraction -']
+        self.qtypes = ['Addition','Times','Division','Subtraction']
         self.difficulties = ['Easy','Intermediate','Hard']
 
 # Function for storing info to file
     def store(self):
         studentfile=open("student_file.txt","a")
-        studentfile.write("\n\nStudent Name: {}\nQuestion Type: {}\nDifficulty: {}\nScore: {}/10"
-                         .format(self.student_name, self.selected_questiontype,
-                                 self.selected_difficulty, self.score))
+        studentfile.write("\n\n{}\n{}\n{}\n{}/10".format(self.student_name,
+                          self.selected_questiontype[0], self.selected_difficulty[0], self.score))
         studentfile.close()
