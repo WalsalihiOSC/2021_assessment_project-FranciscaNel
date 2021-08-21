5565966d10c24b07b4fee124d8149761cee1589f
FranciscaNel <79080152+FranciscaNel@users.noreply.github.com>
2021-08-21 15:23:24 +1200

    Ver.10

--- Francisca_OrmistonMathsStudent.py
+++ Francisca_OrmistonMathsStudent.py
@@ -1,7 +1,7 @@
 # CSC3 2021
 # Ormiston Computing Student Class
 # Francisca Nel
-# Ver 9
+# Ver 10
 
 class Student:
     def __init__(self,sn):
@@ -15,47 +15,37 @@ class Student:
     # question types and difficulties
         self.qtypes = ['Addition','Times','Division','Subtraction']
         self.difficulties = ['Easy','Intermediate','Hard']
+        
+    # 12! text! files! for each question type and difficulty 
+        self.file_paths = [
+        "Text_Files\Addition_easy.txt", "Text_Files\Addition_intermediate.txt", "Text_Files\Addition_hard.txt", 
+        "Text_Files\Subtraction_easy.txt", "Text_Files\Subtraction_intermediate.txt", "Text_Files\Subtraction_hard.txt",
+        "Text_Files\Times_easy.txt", "Text_Files\Times_intermediate.txt", "Text_Files\Times_hard.txt",
+        "Text_Files\Division_easy.txt", "Text_Files\Division_intermediate.txt", "Text_Files\Division_hard.txt"]
 
-        self.high_scores = []
-
-                    # Addition
-        self.file_paths = ["Text_Files\Addition_easy.txt", "Text_Files\Addition_intermediate.txt", "Text_Files\Addition_hard.txt", 
-                    # Subtraction
-                           "Text_Files\Subtraction_easy.txt", "Text_Files\Subtraction_intermediate.txt", "Text_Files\Subtraction_hard.txt",
-                    # Multiplication
-                           "Text_Files\Times_easy.txt", "Text_Files\Times_intermediate.txt", "Text_Files\Times_hard.txt",
-                    # Division
-                           "Text_Files\Division_easy.txt", "Text_Files\Division_intermediate.txt", "Text_Files\Division_hard.txt"]
-
-# Functions for storing info to file (I should be able to simplify these later)
+# Functions for storing info to file depending on difficulty
     def store_addition(self):
-            if self.selected_difficulty[0] == 'Easy':
-                self.writef(0)
-            elif self.selected_difficulty[0] == 'Intermediate':
-                self.writef(1)
-            elif self.selected_difficulty[0] == 'Hard':
-                self.writef(2)
+            if self.selected_difficulty[0] == 'Easy': self.writef(0)
+            elif self.selected_difficulty[0] == 'Intermediate': self.writef(1)
+            elif self.selected_difficulty[0] == 'Hard': self.writef(2)
     def store_subtraction(self):
-            if self.selected_difficulty[0] == 'Easy':
-                self.writef(3)
-            elif self.selected_difficulty[0] == 'Intermediate':
-                self.writef(4)
-            elif self.selected_difficulty[0] == 'Hard':
-                self.writef(5)
+            if self.selected_difficulty[0] == 'Easy': self.writef(3)
+            elif self.selected_difficulty[0] == 'Intermediate': self.writef(4)
+            elif self.selected_difficulty[0] == 'Hard': self.writef(5)
     def store_multiplication(self):
-            if self.selected_difficulty[0] == 'Easy':
-                self.writef(6)
-            elif self.selected_difficulty[0] == 'Intermediate':
-                self.writef(7)
-            elif self.selected_difficulty[0] == 'Hard':
-                self.writef(8)
+            if self.selected_difficulty[0] == 'Easy': self.writef(6)
+            elif self.selected_difficulty[0] == 'Intermediate': self.writef(7)
+            elif self.selected_difficulty[0] == 'Hard': self.writef(8)
     def store_division(self):
-            if self.selected_difficulty[0] == 'Easy':
-                self.writef(9)
-            elif self.selected_difficulty[0] == 'Intermediate':
-                self.writef(10)
-            elif self.selected_difficulty[0] == 'Hard':
-                self.writef(11)
+            if self.selected_difficulty[0] == 'Easy': self.writef(9)
+            elif self.selected_difficulty[0] == 'Intermediate': self.writef(10)
+            elif self.selected_difficulty[0] == 'Hard': self.writef(11)
+
+    def store(self):
+        if self.selected_questiontype[0] == 'Addition': self.store_addition()
+        elif self.selected_questiontype[0] == 'Times': self.store_multiplication()
+        elif self.selected_questiontype[0] == 'Division': self.store_division()
+        elif self.selected_questiontype[0] == 'Subtraction': self.store_subtraction()
 
 # Function that writes to file and produces list to be used for displaying high scores
     def writef(self,i):
@@ -64,12 +54,15 @@ class Student:
             f.close()
         with open(self.file_paths[i],"r") as f:
             c = f.readlines() 
+
         # splits the list into a tuple for the leaderboard
             content = [tuple(line.strip().split()) for line in c] 
+
         # Sort the text file converted to list into 5 highest scores
             content.sort(key=lambda x : int(x[1])) # sorts from index 1 of tuple
             content.reverse()
             print(content)
+
         # prevent index error by filling in empty tuples until 5 elements in list
             if len(content) > 5:
                 self.high_scores = content[0:5]
@@ -79,18 +72,27 @@ class Student:
                     self.high_scores.append(('',''))       
                 print(self.high_scores)
 
-    def store(self):
-        if self.selected_questiontype[0] == 'Addition': self.store_addition()
-        elif self.selected_questiontype[0] == 'Times': self.store_multiplication()
-        elif self.selected_questiontype[0] == 'Division': self.store_division()
-        elif self.selected_questiontype[0] == 'Subtraction': self.store_subtraction()
+            # ... converting the high scores to seperate lists ...
+                self.lscores = [i[1] for i in self.high_scores]
+                self.lnames = [i[0] for i in self.high_scores]
 
-# Resetting all variables for restart
+            # The leaderboard data
+                self.leaderboard_data = [
+                "placeholder","placeholder",
+                ("Place", "Name", "Score"),
+                ("1st",(self.lnames[0]),self.lscores[0]),
+                ("2nd",(self.lnames[1]),self.lscores[1]),
+                ("3rd",(self.lnames[2]),self.lscores[2]),
+                ("4th",(self.lnames[3]),self.lscores[3]),
+                ("5th",(self.lnames[4]),self.lscores[4])]
+
+# Resetting student data for restart
     def reset_var(self):
         self.selected_questiontype = []
         self.selected_difficulty = []
         self.correct_answers = []
         self.score = None
+
 # Resetting user name for logging out
     def reset_user(self):
         self.student_name = None
